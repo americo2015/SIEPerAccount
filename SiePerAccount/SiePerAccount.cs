ce2e26d9a538796089619d33ba0caa29b65f6522
Americo <Americo@DESKTOP-H5I9IG3>
2017-10-25 14:52:31 +0200

    Solution in Terminal

---
+++ SiePerAccount/SiePerAccount.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace SiePerAccount
+{
+    class SiePerAccount
+    {
+        static void Main(string[] args)
+        {
+            var streamReader = File.OpenText(@"SIE.txt");
+            var accounts = new Dictionary<string, decimal>();
+
+            while (true)
+            {
+                var line = streamReader.ReadLine();
+                if (line == null)
+                    break;
+
+                string pattern = @"(#TRANS) (\d{4}) {.*} (-?\d*.\d*)";
+                var match = Regex.Match(line, pattern);
+
+                if (match.Success)
+                {
+
+                    var accountId = match.Groups[2].Value;
+                    var amount = decimal.Parse(match.Groups[3].Value, CultureInfo.InvariantCulture);
+                    
+                    if (accounts.ContainsKey(accountId))
+                        accounts[accountId] += amount;
+                    else
+                        accounts[accountId] = amount;
+                }
+            }
+            foreach (var entry in accounts.OrderBy(e=>e.Key))
+            {
+                Console.WriteLine($"{entry.Key} {entry.Value.ToString("F2")}");
+            }
+            Console.WriteLine(accounts.Sum(entry => entry.Value));
+            Console.ReadLine(); 
+        }
+    }
+}
